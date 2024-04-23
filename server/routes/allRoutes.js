66af0844688f349316cc7d7b4b15e60a88f41ed5
ChandgudeNishant <nishantchandgude_20mk14@sanjivanicoe.org.in>
2024-04-23 11:01:34 +0530

    Changed the MongoDB url

--- server/routes/allRoutes.js
+++ server/routes/allRoutes.js
@@ -5,8 +5,8 @@ const { createAppointment } = require("../controllers/createAppointment");
 const { getAppointments } = require("../controllers/getAppointments");
 const { signUp, login, profile, deleteProfile} = require('../controllers/createUser');
 
-router.post("/createAppointment", jwtAuthMiddleware, createAppointment);
-router.get('/appointments',jwtAuthMiddleware, getAppointments);
+router.post("/createAppointment", createAppointment);
+router.get('/appointments', getAppointments);
 router.post("/createUser",signUp);
 router.post('/loginUser', login);
 router.get('/profile', profile);
